fe57da205709127c57a6de05746b749d6ee1e681
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2016-08-01 13:31:38 +0200

    Fix test

--- lib/boltex.ex
+++ lib/boltex.ex
@@ -1,13 +1,13 @@
 defmodule Boltex do
   alias Boltex.Bolt
 
-  def test(host, port, query, params \\ %{}) do
+  def test(host, port, query, params \\ %{}, auth \\ nil) do
     {:ok, p}   = :gen_tcp.connect host, port, [active: false, mode: :binary, packet: :raw]
 
     :ok        = Bolt.handshake :gen_tcp, p
     :ok        = Bolt.init :gen_tcp, p, params
 
-    IO.inspect Bolt.run_statement(:gen_tcp, p, query, params)
+    IO.inspect Bolt.run_statement(:gen_tcp, p, query)
   end
 
 end
