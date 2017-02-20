3940cfe35e4489c46d24863b79e27736cbcbb92a
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2017-02-20 14:34:36 +0100

    Add test for normal queries

--- test/boltex/bolt_test.exs
+++ test/boltex/bolt_test.exs
@@ -4,6 +4,24 @@ defmodule BoltexTest do
 
   doctest Boltex
 
+  test "encodes normal params correctly", %{port: port} do
+    query = """
+      UNWIND {largeRange} as i
+      RETURN i
+    """
+
+    params = %{largeRange: Enum.to_list(0..100)}
+
+    [{:success, _} | records] = Bolt.run_statement :gen_tcp, port, query, params
+
+    numbers =
+      records
+      |> List.delete_at(-1)
+      |> Enum.map(fn({:record, [number]}) -> number end)
+
+    assert numbers == Enum.to_list(0..100)
+  end
+
   test "encodes big params correctly", %{port: port} do
     query = """
       UNWIND {largeRange} as i
