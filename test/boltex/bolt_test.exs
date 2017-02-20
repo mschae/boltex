e55b7385f7cfeaf68cf4190d30ba35a2857eb981
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2017-02-20 14:28:12 +0100

    Use CaseTemplate for Integration

--- test/boltex/bolt_test.exs
+++ test/boltex/bolt_test.exs
@@ -1,13 +1,10 @@
 defmodule BoltexTest do
-  use ExUnit.Case
+  use Boltex.IntegrationCase
   alias Boltex.Bolt
+
   doctest Boltex
 
-  test "encodes big params correctly" do
-    {:ok, port} = :gen_tcp.connect neo4j_host(), neo4j_port(), [active: false, mode: :binary, packet: :raw]
-    :ok         = Bolt.handshake :gen_tcp, port
-    :ok         = Bolt.init :gen_tcp, port, {"neo4j", "password"}
-
+  test "encodes big params correctly", %{port: port} do
     query = """
       UNWIND {largeRange} as i
       RETURN i
@@ -15,26 +12,13 @@ defmodule BoltexTest do
 
     params = %{largeRange: Enum.to_list(0..25_000)}
 
-    [{:success, _} | records] = Bolt.run_statement(:gen_tcp, port, query, params, [recv_timeout: 100_000])
+    [{:success, _} | records] = Bolt.run_statement :gen_tcp, port, query, params
+
     numbers =
       records
       |> List.delete_at(-1)
       |> Enum.map(fn({:record, [number]}) -> number end)
 
     assert numbers == Enum.to_list(0..25_000)
-  end
-
-  def neo4j_host do
-    case System.get_env("NEO4J_HOST") do
-      nil  -> 'localhost'
-      host -> String.to_charlist host
-    end
-  end
-
-  def neo4j_port do
-    case System.get_env("NEO4J_PORT") do
-      nil  -> 7687
-      port -> String.to_integer port
-    end
   end
 end
---
+++ test/support/integration_case.ex
@@ -0,0 +1,35 @@
+defmodule Boltex.IntegrationCase do
+  use ExUnit.CaseTemplate
+
+  alias Boltex.Bolt
+
+  setup do
+    uri         = neo4j_uri()
+    port_opts   = [active: false, mode: :binary, packet: :raw]
+    {:ok, port} = :gen_tcp.connect uri.host, uri.port, port_opts
+    :ok         = Bolt.handshake :gen_tcp, port
+    :ok         = Bolt.init :gen_tcp, port, uri.userinfo
+
+    on_exit fn ->
+      :gen_tcp.close port
+    end
+
+    {:ok, port: port}
+  end
+
+  def neo4j_uri do
+    "bolt://neo4j:password@localhost:7687"
+    |> URI.merge(System.get_env("NEO4J_TEST_URL") || "")
+    |> URI.parse()
+    |> Map.update!(:host, &String.to_charlist/1)
+    |> Map.update!(:userinfo, fn
+      "" ->
+        {}
+
+      userinfo ->
+        userinfo
+        |> String.split(":")
+        |> List.to_tuple
+    end)
+  end
+end
